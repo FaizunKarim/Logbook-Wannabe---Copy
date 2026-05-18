c768df382f5b3495978619e44401df924c4df520
Pendukung-OkeGas <muhammadfaizun.23178@mhs.unesa.ac.id>
2026-05-18 13:25:16 +0700

    commit

--- api/profile/update.ts
+++ api/profile/update.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { prisma } from "../_lib/prisma.js";
-import { verifyToken, getTokenFromHeader } from "../_lib/jwt.js.js";
+import { verifyToken, getTokenFromHeader } from "../_lib/jwt.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "PUT") {
